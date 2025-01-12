96021ddcf9bf371537cb4aa4e18f93c77604ddd8
Mehmood-Wattoo <mehmoodwattoo712@gmail.com>
2025-01-12 13:11:11 +0000

    + function

---
+++ ch-4-functions/01-func.js
@@ -0,0 +1,8 @@
+// function exercise
+
+// Write a function addNumbers(a, b) that takes two numbers as arguments and returns their sum.
+
+function addNumbers(a,b) {
+    return a + b
+}
+console.log(addNumbers(5,7));
